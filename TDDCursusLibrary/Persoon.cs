2ae6f4983d96ef65a0c5c71b9d89f5ea6a2490c5
Pascal Yankey <pascalyankey@gmail.com>
2019-01-22 14:51:24 +0100

    test persoon ok

---
+++ TDDCursusLibrary/Persoon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TDDCursusLibrary
+{
+    public class Persoon
+    {
+        private static readonly Regex regex = new Regex("^[a-zA-Z]+$");
+        private List<string> voornamen;
+
+        public Persoon(List<string> voornamen)
+        {
+            if (voornamen.Count > 1)
+            {
+                var teller = 1;
+
+                for (var i = 0; i < voornamen.Count; i++)
+                {
+                    if (!regex.IsMatch(voornamen[i]))
+                        throw new ArgumentException();
+
+                    for (var j = 0; j < voornamen.Count - teller; j++)
+                    {
+                        if (voornamen[i].ToLower() == voornamen[j + teller].ToLower())
+                            throw new ArgumentException();
+                    }
+
+                    teller++;
+                }
+            } else
+            {
+                foreach (var voornaam in voornamen)
+                {
+                    if (!regex.IsMatch(voornaam))
+                        throw new ArgumentException();
+                }
+            }
+
+            this.voornamen = voornamen;
+        }
+
+        public override string ToString()
+        {
+            var teller = 0;
+            var voornaamBuilder = new StringBuilder();
+
+            foreach (var voornaam in voornamen)
+            {
+                teller++;
+                if (teller == voornamen.Count)
+                    voornaamBuilder.Append(voornaam);
+                else
+                    voornaamBuilder.Append(voornaam + " ");
+            }
+
+            return voornaamBuilder.ToString();
+        }
+    }
+}
---
+++ TDDCursusLibraryTest/PersoonTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDCursusLibrary;
+
+namespace TDDCursusLibraryTest
+{
+    [TestClass]
+    public class PersoonTest
+    {
+        [TestMethod]
+        public void EenPersoonMet1VoornaamUitMinstens1NonWhiteSpaceIsOk()
+        {
+            new Persoon(new List<string> { "P" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet1VoornaamUitNietMinstens1NonWhiteSpaceIsNietOk()
+        {
+            new Persoon(new List<string> { " " });
+        }
+
+        [TestMethod]
+        public void EenPersoonMet1VoornaamUitAlfabetIsOk()
+        {
+            new Persoon(new List<string> { "Pascal" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet1VoornaamUitNumeriekIsNietOk()
+        {
+            new Persoon(new List<string> { "123" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet1VoornaamUitAlfanumeriekIsNietOk()
+        {
+            new Persoon(new List<string> { "P4scal" });
+        }
+
+        [TestMethod]
+        public void EenPersoonMet2VerschillendeVoornamenMetBeideUitMinstens1NonWhiteSpaceIsOk()
+        {
+            new Persoon(new List<string> { "P", "Y" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet2DezelfdeVoornamenMetBeideUitMinstens1NonWhiteSpaceIsNietOk()
+        {
+            new Persoon(new List<string> { "P", "P" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet2VerschillendeVoornamenMetEnkel1VoornaamUitMinstens1NonWhiteSpaceIsNietOk()
+        {
+            new Persoon(new List<string> { "P", " " });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet2VoornamenMetBeideNietUitMinstens1NonWhiteSpaceIsNietOk()
+        {
+            new Persoon(new List<string> { " ", " " });
+        }
+
+        [TestMethod]
+        public void EenPersoonMet2VerschillendeVoornamenMetBeideUitAlfabetIsOk()
+        {
+            new Persoon(new List<string> { "Pascal", "Jude" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet2VerschillendeVoornamenMet1VoornaamUitNumeriekIsNietOk()
+        {
+            new Persoon(new List<string> { "Pascal", "123" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet2VerschillendeVoornamenMet1VoornaamUitAlfanumeriekIsNietOk()
+        {
+            new Persoon(new List<string> { "Pascal", "Jud3" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet2VerschillendeVoornamenMetBeideUitNumeriekIsNietOk()
+        {
+            new Persoon(new List<string> { "123", "321" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMet2VerschillendeVoornamenMetBeideUitAlfanumeriekIsNietOk()
+        {
+            new Persoon(new List<string> { "P4scal", "Jud3" });
+        }
+
+        [TestMethod]
+        public void EenPersoonMetMeerDan2VerschillendeVoornamenMetAllemaalUitMinstens1NonWhiteSpaceIsOk()
+        {
+            new Persoon(new List<string> { "P", "J", "G", "S" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenPersoonMetMeerDan2VoornamenMet2DezelfdeVoornaamUitMinstens1NonWhiteSpaceIsOk()
+        {
+            new Persoon(new List<string> { "P", "J", "G", "P" });
+        }
+
+        [TestMethod]
+        public void ToStringMethodKomtOvereenMetVoornamen()
+        {
+            Assert.AreEqual("Pascal Jude", new Persoon(new List<string> { "Pascal", "Jude" }).ToString());
+        }
+
+        [TestMethod]
+        public void ToStringMethodKomtNietOvereenMetVoornamen()
+        {
+            Assert.AreNotEqual("Pascal Jude Grace", new Persoon(new List<string> { "Pascal", "Jude" }).ToString());
+        }
+    }
+}
